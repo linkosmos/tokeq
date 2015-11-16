7b426bb88479155c6d5f807a76b9a97d7425c9d5
Ernestas Poskus <ernestas.poskus@gmail.com>
2015-11-16 20:47:20 +0200

    Add PrettyPrint function

--- helpers.go
+++ helpers.go
@@ -8,10 +8,30 @@ import (
 
 // PrintNode - convenient to use as Tok.Callback to see html.Node dump
 func PrintNode(n *html.Node) {
-	fmt.Println(n)
-	fmt.Println("Parent", n.Parent)
-	fmt.Println("FirstChild", n.FirstChild)
-	fmt.Println("LastChild", n.LastChild)
-	fmt.Println("PrevSibling", n.PrevSibling)
-	fmt.Println("NextSibling", n.NextSibling)
+	fmt.Println(PrettyPrint(n))
+}
+
+// PrintNodes - convenient to use as Tok.Callback to see html.Node siblings & parents
+func PrintNodes(n *html.Node) {
+	fmt.Println(PrettyPrint(n))
+	fmt.Println("Parent", PrettyPrint(n.Parent))
+	fmt.Println("FirstChild", PrettyPrint(n.FirstChild))
+	fmt.Println("LastChild", PrettyPrint(n.LastChild))
+	fmt.Println("PrevSibling", PrettyPrint(n.PrevSibling))
+	fmt.Println("NextSibling", PrettyPrint(n.NextSibling))
+}
+
+// PrettyPrint - pretty print node
+func PrettyPrint(n *html.Node) string {
+	if n == nil {
+		return ""
+	}
+	return fmt.Sprintf(
+		`
+Type: %s
+DataAtom: %s
+Data: %s
+Namespace: %s
+Attr: %s
+`, n.Type, n.DataAtom, n.Data, n.Namespace, n.Attr)
 }
